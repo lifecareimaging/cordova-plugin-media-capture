c7425c69f6ae646fea6ce052d7a6ad0f941c76bb
Jari Keinänen <jari@baa.fi>
2018-09-21 12:42:39 +0300

    typo fix

--- src/ios/CDVCapture.m
+++ src/ios/CDVCapture.m
@@ -262,7 +262,7 @@
                 pickerController.videoMaximumDuration = [duration doubleValue];
             }
             if (videoQuality) {
-                if ([videoquality isEqual:@"high"]) {
+                if ([videoQuality isEqual:@"high"]) {
                     pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
                 } else if ([videoQuality isEqual:@"medium"]) {
                     pickerController.videoQuality = UIImagePickerControllerQualityTypeMedium;
